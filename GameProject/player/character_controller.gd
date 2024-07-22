a3d9a40ad2761722708d17b2a0ed1b84c38d340a
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-07-22 18:53:26 +0200

    jump improvements!
    https://www.youtube.com/watch?v=IOe1aGY6hXA

--- GameProject/player/character_controller.gd
+++ GameProject/player/character_controller.gd
@@ -1,30 +1,43 @@
 class_name Player
 extends CharacterBody3D
 
+#movement
 @export var speed = 5.0
-@export var jump_velocity = 4.5
 
+#rotation
 var current_rotation_y: float = 0;
 @export var rotation_speed: float = 3.0;
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#jump
+@export var jump_height: float = 10;
+@export var jump_time_to_peak: float = 0.5;
+@export var jump_time_to_descent: float = 0.4;
+@onready var jump_velocity: float = (2.0 * jump_height) / jump_time_to_peak;
+@onready var jump_gravity: float = (-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak);
+@onready var fall_gravity: float = (-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent);
+
+#mechanics
 @export var inventory: Inventory;
+@export var interaction_range: Area3D;
 
 func _init():
 	Manager.instance.player = self;
+	
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
 func _physics_process(delta):
 	if not Manager.instance.camera_controller:
 		return;
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+		
+	velocity.y += get_gravity() * delta;
+
+	if is_on_floor():
+		velocity.y = 0;
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = jump_velocity;
 		
 	if Input.is_action_just_pressed("open_inventory"):
 		SceneManager.instance.set_active_scene("inventory", SceneConfig.new())
