a7c20e95c7fdb18839f521da07127333cfe7b84e
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-07-30 15:41:55 +0200

    added script to game scene for wisp control

--- GameProject/scripts/move_to_waypoint.gd
+++ GameProject/scripts/move_to_waypoint.gd
@@ -1,7 +1,17 @@
-extends Node
+class_name Mover extends Node
 
 @export var target: Node3D;
 @export var time_to_destination: float = 1;
 
+@onready var wisp: GPUParticles3D = $Wisp;
+@onready var energy: GPUParticles3D = $Energy;
+
+@export var gradient_white: GradientTexture1D;
+@export var gradient_black: GradientTexture1D;
+
 func _ready():
 	create_tween().tween_property(self, "position", target.position, time_to_destination)
+	
+	
+func set_color(gradient: GradientTexture1D):
+	(wisp.process_material as ParticleProcessMaterial).color_ramp = gradient;
---
+++ GameProject/scripts/wisp_controller.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var wisps: Array[Mover] = []
+
+func transition_color(color: Color):
+	for wisp in wisps:
+		if color == Color.BLACK:
+			wisp.set_color(wisp.gradient_black);
+		elif color == Color.WHITE:
+			wisp.set_color(wisp.gradient_white);
+		else:
+			print("Color provided was not valid")
+		
+func _ready():
+	transition_color(Color.BLACK)
+	pass
